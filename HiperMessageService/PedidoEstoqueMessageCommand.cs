c748c40957bab9555c92029a98b518d58c865c73
thiagoh2005 <thiagoh2005@hotmail.com>
2026-01-20 00:27:11 -0300

    Adicionar arquivos de projeto.

---
+++ HiperMessageService/PedidoEstoqueMessageCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace HiperMessageService
+{
+    public class PedidoEstoqueMessageCommand
+    {
+        public Guid Id { get; set; }
+        public string Responsavel { get; set; }
+        public string Descricao { get; set; }
+        public Guid ItemId { get; set; }
+        public string Item { get; set; }
+        public DateTime DataEntregue { get; set; }
+        public int Unidades { get; set; }
+    }
+}
---
+++ HiperMessageService/Program.cs
@@ -0,0 +1,60 @@
+﻿using HiperMessageService;
+using Npgsql;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var hiperMessageConnStr = "Server=localhost;Port=5433;Database=HiperMessageDB;Username=postgres;Password=1234";
+await using var hiperMessageDB = NpgsqlDataSource.Create(hiperMessageConnStr);
+var messageSql = @"
+    INSERT INTO pedido_estoque_message (
+    id,
+    responsavel,
+    descricao,
+    item_id,
+    item,
+    data_entregue,
+    unidades
+)
+VALUES (
+    @Id,
+    @Responsavel,
+    @Descricao,
+    @ItemId,
+    @Item,
+    @DataEntregue,
+    @Unidades
+);";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "receberPedidoEstoque", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, eventeArgs) =>
+{
+    var body = eventeArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var pedidoCommand = JsonSerializer.Deserialize<PedidoEstoqueMessageCommand>(message);
+
+    Console.WriteLine($"Recebido: {message}");
+    await using (var cmd = hiperMessageDB.CreateCommand(messageSql))
+    {
+        cmd.Parameters.AddWithValue("Id", pedidoCommand.Id);
+        cmd.Parameters.AddWithValue("Responsavel", pedidoCommand.Responsavel);
+        cmd.Parameters.AddWithValue("Descricao", pedidoCommand.Descricao);
+        cmd.Parameters.AddWithValue("ItemId", pedidoCommand.ItemId);
+        cmd.Parameters.AddWithValue("Item", pedidoCommand.Item);
+        cmd.Parameters.AddWithValue("DataEntregue", pedidoCommand.DataEntregue);
+        cmd.Parameters.AddWithValue("Unidades", pedidoCommand.Unidades);
+        await cmd.ExecuteNonQueryAsync();
+    }
+    Console.WriteLine("Persistido no banco");
+};
+
+await channel.BasicConsumeAsync("receberPedidoEstoque", autoAck: true, consumer: consumer);
+
+Console.ReadLine();
